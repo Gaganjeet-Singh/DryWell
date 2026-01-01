f636a39dcc8c02ce6a24752fef0723df236caaf7
Gaganjeet Singh <gagandhiman870@gmail.com>
2026-01-01 20:03:23 +0530

    resolve horizontal scroll error

--- frontend/src/landingPage/Contact/Hero.js
+++ frontend/src/landingPage/Contact/Hero.js
@@ -1,24 +1,16 @@
-import React from "react";
+import React from 'react';
 
 function Hero() {
-  return (
-    <section id="HeroService" className="py-5">
-      <div className="container-fluid px-0">
-        <div className="row justify-content-center text-center m-0">
-          <div className="col-12 col-md-10 col-lg-8 px-3">
-
-            <p className="fs-6 mb-2">Get In Touch</p>
-
-            <p className="fs-5 mb-0">
-              Have questions? We'd love to hear from you. Send us a message and
-              we'll respond as soon as possible.
-            </p>
-
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    return ( 
+        <section id ="HeroService">
+            <div className='col-12 px-3 px-md-0'>
+                <div className='row text-center'>
+                    <p className='fs-6'>Get In Touch</p>
+                    <p className='fs-5'>Have questions? We'd love to hear from you. Send us a message and we'll respond as <br/>soon as possible.</p>
+                </div>
+            </div>
+        </section>
+     );
 }
 
 export default Hero;
