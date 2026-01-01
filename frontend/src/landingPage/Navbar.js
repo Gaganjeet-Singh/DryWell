7fb441afb5fc80aa8bac4194eda462de37094022
Gaganjeet Singh <gagandhiman870@gmail.com>
2026-01-01 18:36:39 +0530

    main logo clickable

--- frontend/src/landingPage/Navbar.js
+++ frontend/src/landingPage/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
     return ( 
         <nav class="navbar navbar-expand-lg border-bottom">
             <div class="container p-2">
-                <a class="navbar-brand" href="#" id = "NavLogo">DryWELL</a>
+                <Link class="navbar-brand" to ="/" id = "NavLogo">DryWELL</Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
