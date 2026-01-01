d51d51782da3b450bd4bd1df148546b6cf0795e6
Gaganjeet Singh <gagandhiman870@gmail.com>
2026-01-01 20:08:51 +0530

    footer responsiveness improved

--- frontend/src/landingPage/Footer.js
+++ frontend/src/landingPage/Footer.js
@@ -1,41 +1,81 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-    return ( 
-        <footer style = {{backgroundColor : "#F4F7FA"}}>
-            <div className='container '>
-                <div className='row  mb-4 p-5 border-bottom'>
-                    <div className='col-3'>
-                        <h4 style = {{color:"#2E2E2E"}}>DryWELL Laundry</h4>
-                        <p style = {{color:"#2E2E2E"}}>Professional laundry and dry-cleaning service. Clean clothes, fresh start.</p>
-                    </div>
-                    <div className='col-3'>
-                        <h5 style = {{color:"#2E2E2E"}}>Quick Links</h5>
-                        <a style = {{textDecoration:"none", color:"#0077B6"}} href =''>Home</a><br/>
-                        <a style = {{textDecoration:"none",color:"#0077B6"}} href =''>Services</a><br/>
-                        <a style = {{textDecoration:"none",color:"#0077B6"}} href =''>Pricing</a><br/>
-                        <a style = {{textDecoration:"none",color:"#0077B6"}} href =''>About Us</a>
-                    </div>
-                    <div className='col-3'>
-                        <h5 style = {{color:"#2E2E2E"}}>Services</h5>
-                        <a style = {{textDecoration:"none",color:"#0077B6"}} href = ''>Wash & Fold</a><br/>
-                        <a style = {{textDecoration:"none",color:"#0077B6"}} href = ''>Dry Cleaning</a><br/>
-                        <a style = {{textDecoration:"none",color:"#0077B6"}} href = ''>Ironing Service</a><br/>
-                        <a style = {{textDecoration:"none",color:"#0077B6"}} href = ''>Stain Removal</a>
-                    </div>
-                    <div className='col-3'>
-                        <h5 style = {{color:"#2E2E2E"}}>Contact Us</h5>
-                        <p style = {{color:"#2E2E2E"}}><i class="fa fa-phone" aria-hidden="true"></i> +1 (555) 123-4567</p>
-                        <p style = {{color:"#2E2E2E"}}><i class="fa fa-envelope-o" aria-hidden="true"></i>hello@drywell.com</p>
-                        <p style = {{color:"#2E2E2E"}}><i class="fa fa-map-marker" aria-hidden="true"></i>123 Clean Street, Fresh City, FC 12345</p>
-                    </div>
-                </div>
-                <div className='row mt-3  text-center'>
-                    <p style = {{color:"#2E2E2E"}}>© 2025 DryWELL Laundry. All rights reserved.</p>
-                </div>
-            </div>
-        </footer>
-     );
+  return (
+    <footer style={{ backgroundColor: "#F4F7FA" }}>
+      <div className="container py-5">
+
+        {/* TOP FOOTER */}
+        <div className="row g-4 border-bottom pb-4">
+
+          {/* BRAND */}
+          <div className="col-12 col-md-6 col-lg-3">
+            <h4 style={{ color: "#2E2E2E" }}>DryWELL Laundry</h4>
+            <p style={{ color: "#2E2E2E" }}>
+              Professional laundry and dry-cleaning service. Clean clothes,
+              fresh start.
+            </p>
+          </div>
+
+          {/* QUICK LINKS */}
+          <div className="col-6 col-md-3 col-lg-3">
+            <h5 style={{ color: "#2E2E2E" }}>Quick Links</h5>
+            <ul className="list-unstyled">
+              <li><a className="footer-link" href="#">Home</a></li>
+              <li><a className="footer-link" href="#">Services</a></li>
+              <li><a className="footer-link" href="#">Pricing</a></li>
+              <li><a className="footer-link" href="#">About Us</a></li>
+            </ul>
+          </div>
+
+          {/* SERVICES */}
+          <div className="col-6 col-md-3 col-lg-3">
+            <h5 style={{ color: "#2E2E2E" }}>Services</h5>
+            <ul className="list-unstyled">
+              <li><a className="footer-link" href="#">Wash & Fold</a></li>
+              <li><a className="footer-link" href="#">Dry Cleaning</a></li>
+              <li><a className="footer-link" href="#">Ironing Service</a></li>
+              <li><a className="footer-link" href="#">Stain Removal</a></li>
+            </ul>
+          </div>
+
+          {/* CONTACT */}
+          <div className="col-12 col-md-6 col-lg-3">
+            <h5 style={{ color: "#2E2E2E" }}>Contact Us</h5>
+
+            <p className="d-flex align-items-start gap-2 mb-2">
+              <i className="fa fa-phone mt-1" />
+              <span>+1 (555) 123-4567</span>
+            </p>
+
+            <p className="d-flex align-items-start gap-2 mb-2">
+              <i className="fa fa-envelope mt-1" />
+              <span>hello@drywell.com</span>
+            </p>
+
+            <p className="d-flex align-items-start gap-2 mb-0">
+              <i className="fa fa-map-marker mt-1" />
+              <span>
+                123 Clean Street, Fresh City,<br />
+                FC 12345
+              </span>
+            </p>
+          </div>
+
+        </div>
+
+        {/* BOTTOM FOOTER */}
+        <div className="row pt-4">
+          <div className="col-12 text-center">
+            <p className="mb-0" style={{ color: "#2E2E2E" }}>
+              © 2025 DryWELL Laundry. All rights reserved.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
